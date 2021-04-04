9f8def11e754b886f8294b246388cf384c7e5f16
ruimandrade <73177181+ruimandrade@users.noreply.github.com>
2021-04-04 13:37:01 +0100

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd 
+
+st.write("""
+# Simple Stock Price App
+
+Shown ara the stock **closeing price** and ***volume*** of Tesla!
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2005-01-02', end='2021-03-30')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
